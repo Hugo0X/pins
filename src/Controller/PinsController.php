f4fabf3c288516e83fbd88b5c57c2bb156ca3774
Hugo0X <hugo95martin@gmail.com>
2021-01-28 14:08:45 +0100

    add voter and fix security

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -13,6 +13,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Knp\Component\Pager\PaginatorInterface;
+// use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 
 
@@ -80,13 +82,10 @@ class PinsController extends AbstractController
 
     /**
      * @Route("/pins/{id<[0-9]+>}/edit", name="app_pins_edit", methods={"GET", "PUT"})
+     * @IsGranted("PIN_MANAGE", subject="pin")
     */
     public function edit(Pin $pin, Request $request): Response
     {
-        if($this->getUser() != $pin->getUser()){
-            throw $this->createAccessDeniedException();
-        }
-
         $form =$this->createForm(PinType::class, $pin, [
             'method' => 'PUT'
         ]);
@@ -112,13 +111,10 @@ class PinsController extends AbstractController
    
      /**
      * @Route("/pins/{id<[0-9]+>}", name="app_pins_delete", methods={"DELETE"})
+     * @IsGranted("PIN_MANAGE", subject="pin")
      */
     public function delete(Request $request, Pin $pin, EntityManagerInterface $em): Response
-    {
-        if($this->getUser() != $pin->getUser()){
-            throw $this->createAccessDeniedException();
-        }
-        
+    {    
         if ($this->isCsrfTokenValid('pin_deletion_' . $pin->getId(), $request->request->get('csrf_token'))) {
             $em->remove($pin);
             $em->flush();
---
+++ src/Security/Voter/PinVoter.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Security\Voter;
+
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class PinVoter extends Voter
+{
+    protected function supports($attribute, $subject)
+    {
+        return in_array($attribute, ['PIN_MANAGE']) && $subject instanceof \App\Entity\Pin;
+    }
+
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        $user = $token->getUser();
+        // if the user is anonymous, do not grant access
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        // ... (check conditions and return true to grant permission) ...
+        switch ($attribute) {
+            case 'PIN_MANAGE':
+                return $user->isVerified() && $user == $subject->getUser();
+        }
+
+        return false;
+    }
+}
